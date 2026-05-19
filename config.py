64646761b42696c7206ecba471ce89c7f3c823e0
wexquize <stetsenkovlad313@gmail.com>
2026-05-19 15:24:42 +0300

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
 
-BOT_TOKEN = os.getenv("8673691254:AAF1TKhm8G31oGGPA3KvdZPttboVF2CoS8A", "")
-ADMIN_ID = int(os.getenv("7643929600", "0"))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
 REFERRALS_FOR_PREMIUM = 3
+
 MINIAPP_URL = "https://wexquize.github.io/wexbot/"
 API_PORT = int(os.getenv("PORT", "8080"))
+
+BOT_USERNAME = os.getenv("BOT_USERNAME", "wexlogedbot")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
