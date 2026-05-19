94ef67f0a2ab3e6db23d8239c78bd41b9d3f88f8
wexquize <stetsenkovlad313@gmail.com>
2026-05-19 16:25:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 REFERRALS_FOR_PREMIUM = 3
 MINIAPP_URL = "https://wexquize.github.io/wexbot/"
 API_PORT = int(os.getenv("PORT", "8080"))
-BOT_USERNAME = os.getenv("BOT_USERNAME", "wexbot")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "wexlogedbot")
